8a968625f95871558eee980a723f2fcf30da7122
kamran.n.karimov <kamran.n.karimov@abb-bank.az>
2022-10-02 18:15:41 +0400

    added vulnerable code

--- src/main/java/org/lab4/Main.java
+++ src/main/java/org/lab4/Main.java
@@ -1,21 +1,11 @@
 package org.lab4;
 
-import java.lang.reflect.Field;
+import java.io.IOException;
 
 public class Main {
     static User kamran = new User("kamran", "karimov", 20, Gender.MALE, Role.ADMIN);
 
-    void getPrivateNameFieldOfUserWithReflection(User user) throws NoSuchFieldException, IllegalAccessException {
-        Field name = User.class.getDeclaredField("name");
-
-        name.setAccessible(true);
-
-        String fieldValue = (String) name.get(user);
-
-        System.out.println("name field from User class object" + fieldValue);
-    }
-
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) {
         System.out.println(kamran.getName());
         System.out.println(kamran.getSurname());
         System.out.println(kamran.getAge());
@@ -23,5 +13,9 @@ public class Main {
         System.out.println(kamran.getBirthYear());
         System.out.println(kamran.getGender());
         System.out.println(kamran.getRole());
+    }
+
+    void vulnerableCode(String cmd, String arg) throws IOException {
+        Runtime.getRuntime().exec(cmd + " " + arg);
     }
 }
